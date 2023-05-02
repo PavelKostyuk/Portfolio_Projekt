ddf263195e9c80513ff18a2ec30f27efadc69fde
Pavel Kostyuk <pavel_kostyuk@hotmail.com>
2023-05-02 16:34:25 +0200

    Fungerande portfolio -behöver arbeta med relevant text

--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import projects
 import projects.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', projects.views.home, name='home'),
-    path('about', projects.views.about, name='about'),
-]
+    path('detail/<int:project_id>', projects.views.detail, name='detail')
+
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Portfolio/projects/views.py
+++ Portfolio/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 # Create your views here.
@@ -6,5 +6,6 @@ def home(request):
     projects = Project.objects
     return render(request, 'projects/home.html', {'projects':projects})
 
-def about(request):
-    return render(request, 'projects/about.html')
+def detail(request, project_id):
+    project_detail = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/detail.html', {'project':project_detail})
