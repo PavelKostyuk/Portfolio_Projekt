b3c7c80be302c5a9b9da996d6634385a4cfd9ab0
Pavel Kostyuk <pavel_kostyuk@hotmail.com>
2023-05-04 15:15:12 +0200

    ''

--- Portfolio/Portfolio/wsgi.py
+++ Portfolio/Portfolio/wsgi.py
@@ -1,10 +1,8 @@
-import os 
+import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Check for the PRODUCTION environment variable to see if we are running in Azure App Service
-# If so, then load the settings from production.py
-settings_module = 'portfolio.production' if 'PRODUCTION' in os.environ else 'portfolio.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
 
 application = get_wsgi_application()
