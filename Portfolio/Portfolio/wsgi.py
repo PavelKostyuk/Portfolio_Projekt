cf34c50b0a199303d61e167e6996e07f88cacc15
Pavel Kostyuk <pavel_kostyuk@hotmail.com>
2023-05-11 02:18:04 +0200

    wsgi modified and tested

--- Portfolio/Portfolio/wsgi.py
+++ Portfolio/Portfolio/wsgi.py
@@ -1,7 +1,10 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(BASE_DIR, 'Portfolio'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
+
+application = get_wsgi_application()
