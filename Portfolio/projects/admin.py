b9146765c4471be30d41ce27e3bcc1b569b29fe4
Pavel Kostyuk <pavel_kostyuk@hotmail.com>
2023-05-15 21:57:11 +0200

    modified admin.py to use ckeditor

--- Portfolio/projects/admin.py
+++ Portfolio/projects/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.db import models
+from ckeditor.widgets import CKEditorWidget
 from .models import Project
-# Register your models here.
+from django import forms
+
+class ProjectAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['title']
     search_fields = ['title', 'description']
     list_filter = ['title']
+    form = ProjectAdminForm
+
+    # Override the formfield for the description field to use CKEditor
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget()},
+    }
