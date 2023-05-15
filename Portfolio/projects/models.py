3fd1b383c43ca2954383e4508aa631ce491c5db6
Pavel Kostyuk <pavel_kostyuk@hotmail.com>
2023-05-15 23:23:58 +0200

    ''

--- Portfolio/projects/models.py
+++ Portfolio/projects/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
+from django import forms    
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
 # Create your models here
@@ -7,9 +10,16 @@ class Project(models.Model):
     title = models.CharField(max_length=250)
     description = RichTextUploadingField()
     project_image = models.ImageField(upload_to='media')
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'project_image']
+        widgets = {
+            'description': CKEditorUploadingWidget(),
+        }
     
 
-
- 
 def __str__(self):
     return self.title
